af68de0a2e082d0b44e60256c98bf3dd279d16df
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-05-29 23:15:57 +0200

    Update project name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import type { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  description: 'Next 14 Starter',
-  title: 'Next 14 starter with ESLint, Prettier, and Tailwind',
+  description: 'React Scroll Highlight',
+  title: 'Next 14 app with React Scroll Highlight',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
